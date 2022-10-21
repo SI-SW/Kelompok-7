89a8822c55b543a4e7d77340b15ddd79010683c4
Bagas Arya <92568986+BagasArya12201728@users.noreply.github.com>
2022-10-21 20:02:34 +0700

    Tugas_Kelompoku
    B_ 2741006_ Bagas Arya Saputra_STMIK EL Rahma Yogyakarta

---
+++ vue-argon-dashboard-main/src/views/Dashboard/ToDo.vue
@@ -0,0 +1,33 @@
+<script>
+import { mapState } from 'pinia';
+import d$todo from '@/stores/dashboard/todo';
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'ToDo',
+    computed: {
+        ...mapState(d$todo, ['g$list']),
+    },
+    methods: {
+        ...mapActions(d$todo, ['a$list']),
+        async getList() {
+            try {
+                await this.a$list();
+                console.error('methods getList error', e);
+            } catch (e) {
+                
+            }
+        }
+    },
+    async created() {
+        await this.getList();
+    },
+};
+</script>
+<template>
+    <div>
+        <ol>
+           <li v-for="(item, index) in g$list" :key="index">{{ item }}</li> 
+        </ol>
+    </div>
+</template>
